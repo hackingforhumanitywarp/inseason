6a677f4d7346a1d42148d49103851b9dd4ed3af4
ArielleMurad <ariellem99@gmail.com>
2019-05-05 11:21:14 -0700

    router for produce

--- src/Produce.js
+++ src/Produce.js
@@ -1,8 +1,7 @@
 import React from "react";
-import AppRouter from "./AppRouter";
-import logo from "./logo.svg";
 import ButtonToolbar from "react-bootstrap/ButtonToolbar";
 import Dropdown from "react-bootstrap/Dropdown";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class Produce extends React.Component {
     render() {
@@ -21,25 +20,48 @@ class Produce extends React.Component {
                     </Dropdown.Menu>
                 </Dropdown>
 
-                <Dropdown>
+                <Router>
+                <div className="Months">
+                    <nav>
+                        <Dropdown>
                     <Dropdown.Toggle variant="success" id="dropdown-month">
                         Month
                     </Dropdown.Toggle>
                     <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1">January</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">February</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">March</Dropdown.Item>
-                        <Dropdown.Item href="#/action-4">April</Dropdown.Item>
-                        <Dropdown.Item href="#/action-5">May</Dropdown.Item>
-                        <Dropdown.Item href="#/action-6">June</Dropdown.Item>
-                        <Dropdown.Item href="#/action-7">July</Dropdown.Item>
-                        <Dropdown.Item href="#/action-8">August</Dropdown.Item>
-                        <Dropdown.Item href="#/action-9">September</Dropdown.Item>
-                        <Dropdown.Item href="#/action-10">October</Dropdown.Item>
-                        <Dropdown.Item href="#/action-11">November</Dropdown.Item>
-                        <Dropdown.Item href="#/action-12">December</Dropdown.Item>
+                        <Link to ="/jan">January</Link>
+                        <Link to ="/feb">February</Link>
+                        <Link to ="/mar">March</Link>
+                        <Link to ="/apr">April</Link>
+                        <Link to ="/may">May</Link>
+                        <Link to ="/jun">June</Link>
+                        <Link to ="/jul">July</Link>
+                        <Link to ="/aug">August</Link>
+                        <Link to ="/sep">September</Link>
+                        <Link to ="/oct">October</Link>
+                        <Link to ="/nov">November</Link>
+                        <Link to ="/dec">December</Link>
                     </Dropdown.Menu>
-                </Dropdown>
+                        </Dropdown>
+                    </nav>
+
+                    <Route path="/jan" component={Jan} />
+                    <Route path="/feb" component={Feb} />
+                    <Route path="/mar" component={Mar} />
+                    <Route path="/apr" component={Apr} />
+                    <Route path="/may" component={May} />
+                    <Route path="/jun" component={Jun} />
+                    <Route path="/jul" component={Jul} />
+                    <Route path="/aug" component={Aug} />
+                    <Route path="/sep" component={Sep} />
+                    <Route path="/oct" component={Oct} />
+                    <Route path="/nov" component={Nov} />
+                    <Route path="/dec" component={Dec} />
+
+
+
+                </div>
+                </Router>
+
                 </div>
                 <ButtonToolbar/>
 
@@ -47,5 +69,45 @@ class Produce extends React.Component {
         );
     }
 }
+
+function Jan() {
+    return <h2>January</h2>;
+}
+
+function Feb() {
+    return <h2>January</h2>;
+}
+function Mar() {
+    return <h2>January</h2>;
+}
+function Apr() {
+    return <h2>January</h2>;
+}
+function May() {
+    return <h2>January</h2>;
+}
+function Jun() {
+    return <h2>January</h2>;
+}
+function Jul() {
+    return <h2>January</h2>;
+}
+function Aug() {
+    return <h2>January</h2>;
+}
+function Sep() {
+    return <h2>January</h2>;
+}
+function Oct() {
+    return <h2>January</h2>;
+}
+function Nov() {
+    return <h2>January</h2>;
+}
+function Dec() {
+    return <h2>January</h2>;
+}
+
+
 
 export default Produce;
